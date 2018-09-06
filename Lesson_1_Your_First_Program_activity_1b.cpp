5ce6b78dfb5c51df4e63ff87ed24adafdb5bcf07
thobishi@gmail.com <thobishi@gmail.com>
2018-09-06 21:23:21 +0200

    Subactivity 1.b.iii  completed

--- Lesson_1_Your_First_Program_activity_1b.cpp
+++ Lesson_1_Your_First_Program_activity_1b.cpp
@@ -1,16 +1,20 @@
 /*
-    Subactivity 1.b.i
+    Subactivity 1.b.iii
     ------------------------
-    As you can see, the compiler doesn’t mind whether a program is written
-    in bad programming style, but for us humans, it is more difficult to read and understand what the various
-    parts of a program do when it is written in bad style
+    What does 'using namespace std'; mean?
+
+    - You can leave 'using namespace std' line out of your program but you need to adjust the program
+    by prefixing 'cout' with 'std::'
+    - As explained above, 'cout' is defined in the 'iostream' header file.
+    - All the names defined in 'iostream' it belong to a namespace called 'std'.
+
 */
 #include <iostream>
 
-using namespace std;
+// using namespace std;
 
 int main()
 {
-    cout << "Hello, World";
+    std::cout << "Hello, World";
     return 0;
 }
