09dced41affeae69b6789b287b08fc723bb6b2a1
thobishi@gmail.com <thobishi@gmail.com>
2018-09-06 22:02:12 +0200

    Activity 1 c completed

---
+++ Subactivity_1_c_i.cpp
@@ -0,0 +1,27 @@
+/*
+    Subactivity 1.c.i
+    -------------------------
+    Edit the program of Subactivity 1.b.i to look like this:
+    #include <iostream>
+
+    using namespace std;
+
+    int main( )
+    {
+        //Display a message
+        cout << "Hello world";
+        return 0;
+    }
+*/
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Dispalce a messga
+    cout << "Hello, World";
+    return 0;
+}
