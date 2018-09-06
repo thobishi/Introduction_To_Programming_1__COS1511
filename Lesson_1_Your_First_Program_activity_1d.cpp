898ce9a5c672a3985990776571193447e3ae2f0f
thobishi@gmail.com <thobishi@gmail.com>
2018-09-06 22:11:54 +0200

    Activity 1d completed

---
+++ Lesson_1_Your_First_Program_activity_1d.cpp
@@ -0,0 +1,23 @@
+/*
+    Subactivity 1.d
+    -------------------------
+    Write a program to display the following on the screen:
+        Hello, everybody!
+        My name is YourName.
+        Goodbye.
+    Where YourName appears, insert your own name.
+*/
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Dispalce a messga
+    cout << "Hello, everybody!" << endl;
+    cout << "My name is Thobishi" << endl;
+    cout << "Goodbye." << endl;
+    return 0;
+}
---
+++ Subactivity_1_d_i.cpp
@@ -0,0 +1,23 @@
+
+/*
+    Subactivity 1.d.i
+    -------------------------
+    Predict what the output of the following program will be:
+*/
+
+
+//Displays Hello world on the screen
+#include <iostream>
+
+using namespace std;
+
+int main( )
+{
+    cout << "Hello";
+    cout << "world";
+    return 0;
+}
+
+/*
+    The program when run it will display 'Helloworld' on the console
+*/
