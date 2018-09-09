b008db63f4e3c98e4b14eb0e8b32c3ef1c5c249a
thobishi@gmail.com <thobishi@gmail.com>
2018-09-09 11:09:39 +0200

    Excercise

---
+++ Lesson_1_Excercises.cpp
@@ -0,0 +1,8 @@
+/*
+    Exercise 1.1
+    ---------------------
+    Compare the description of the program structure that we have just given with the program we gave when
+    describing the purpose of this lesson. What part of the program corresponds to DescriptiveComment? What
+    parts correspond to StandardHeaderFile and StatementSequence?
+    
+*/
